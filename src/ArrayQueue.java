82f9256aa71e0df0469a43f71457f1b2058c517a
Luping Lin <lupinglin@gmail.com>
2018-07-09 23:01:08 -0700

    replace original ArrayQueue.

--- src/ArrayQueue.java
+++ src/ArrayQueue.java
@@ -1,108 +1,74 @@
 import java.util.LinkedList;
 
-/**
- * Implement a queue using int[] with size limit to 10. queue size is not limited.
- * @author luping
- *
- */
 public class ArrayQueue {
-	private static final int ARRAY_SIZE = 10;
-	private int headIndex = 0;
-	private int tailIndex = 0;
-	LinkedList<Node> nodes = new LinkedList<>();
-	
-	public class ArrayQueueException extends Exception {
-		private static final long serialVersionUID = 1L;
+	private static int ARR_SIZE = 5;
+	static class Node {
+		int[] arr;
+		int itemCount;
 
-		public ArrayQueueException(String msg) {
-			super(msg);
+		Node() {
+			arr = new int[ARR_SIZE];
+			itemCount = 0;
 		}
 	}
-	
-	class Node {
-		int[] arr = new int[ARRAY_SIZE];
-		int validValueCount = 0;
-		
-		int get(int index) throws ArrayQueueException {
-			if (validValueCount > 0) {
-				return arr[index];				
-			}
-			throw new ArrayQueueException("Cannot peek an empty queue");
-		}
-		
-		int remove(int index) throws ArrayQueueException {
-			if (validValueCount > 0) {
-				validValueCount--;
-				return arr[index];				
-			}
-			throw new ArrayQueueException("Cannot dequeue an empty queue");
-		}
-		
-		void add(int index, int val) {
-			arr[index] = val;
-			validValueCount++;
-		}
-		
-		void print() {
-			for (int i = 0; i < ARRAY_SIZE; i++) {
-				System.out.print(arr[i] + " ");
-			}
-			System.out.println();
-		}
-	}
-	
-	public void enqueue(int val) {
-		synchronized(nodes) {
-			if (nodes.isEmpty() ){
-				nodes.add(new Node());
-				headIndex = 0;
-				tailIndex = 0;
-			} else if (tailIndex == ARRAY_SIZE) {
-				nodes.add(new Node());
-				tailIndex = 0;
-			}
 
-			Node tailNode = nodes.getLast();
-			tailNode.add(tailIndex++, val);
+	private final LinkedList<Node> nodes;
+	int head = 0, tail = 0;
+
+	public ArrayQueue() {
+		nodes = new LinkedList<>();
+	}
+
+	public void enqueue(int i) {
+		if (nodes.isEmpty() || nodes.getLast().itemCount == ARR_SIZE) {
+			nodes.add(new Node());
+			tail = 0;
 		}
+		Node last = nodes.getLast();
+		last.arr[tail++] = i;
+		last.itemCount++;
 	}
 
 	public int dequeue() throws ArrayQueueException {
-		synchronized(nodes) {
-			if (nodes.isEmpty()) {
-				throw new ArrayQueueException("Cannot dequeue an empty queue");
-			}
-			
-			Node headNode = nodes.getFirst();
-			int val = headNode.remove(headIndex++);
-			if (headIndex == ARRAY_SIZE) {
-				nodes.removeFirst();
-				headIndex = 0;
-			}
-			
-			if (nodes.size() == 1 && headIndex > tailIndex) {
-				tailIndex = headIndex;
-			}
-			return val;
+		if (nodes.isEmpty() || nodes.getFirst().itemCount == 0) {
+			throw new ArrayQueueException("cannot deqeue empty queue");
 		}
-	}
-	
-	public int peek() throws ArrayQueueException{
-		if (nodes.isEmpty() ) {
-			throw new ArrayQueueException("Cannot peek an empty queue");
+		Node first = nodes.getFirst();
+		int ret = first.arr[head++];
+		first.itemCount--;
+		if (first.itemCount == 0) {
+			nodes.removeFirst();
+			head = 0;
 		}
-		Node headNode = nodes.getFirst();
-		int val = headNode.get(headIndex);
-		return val;
+		return ret;
 	}
-	
-	private void printQueue() {
+
+	public int peek() throws ArrayQueueException {
+		if (nodes.isEmpty() || nodes.getFirst().itemCount == 0) {
+			throw new ArrayQueueException("cannot deqeue empty queue");
+		}
+		Node first = nodes.getFirst();
+		return first.arr[head];
+	}
+
+	public void printQueue() {
 		for (int i = 0; i < nodes.size(); i++) {
 			Node node = nodes.get(i);
-			node.print();
+			int start = (i == 0) ? head : 0;
+			int end = (i == nodes.size() - 1) ? tail : node.itemCount;
+
+			for (int j = start; j < end; j++) {
+				System.out.print(node.arr[j] + " ");
+			}
 		}
 	}
-	
+
+	static class ArrayQueueException extends Exception {
+		ArrayQueueException(String msg) {
+			super(msg);
+		}
+	}
+
 	public static void main(String[] args) throws ArrayQueueException {
 		ArrayQueue q = new ArrayQueue();
 		for (int i = 0; i < 15; i++) {
@@ -110,13 +76,13 @@ public class ArrayQueue {
 		}
 		System.out.println("********* equeue 0-15 ********");
 		q.printQueue();
-		
+
 		System.out.println("********* dequeue 0-5 ********");
 		for (int i = 0; i < 5; i++) {
-			System.out.println("dequeue expect: " + i + ", actual: " + q.dequeue());			
+			System.out.println("dequeue expect: " + i + ", actual: " + q.dequeue());
 		}
 		q.printQueue();
-			
+
 		System.out.println("********* peek ********");
 		System.out.println("peek expect: 5, actual: " + q.peek());
 
@@ -128,7 +94,7 @@ public class ArrayQueue {
 
 		System.out.println("*********dequeue  5-20 ********");
 		for (int i = 5; i < 20; i++) {
-			System.out.println("dequeue expect: " + i + ", actual: " + q.dequeue());			
+			System.out.println("dequeue expect: " + i + ", actual: " + q.dequeue());
 		}
 		q.printQueue();
 
@@ -137,7 +103,7 @@ public class ArrayQueue {
 		} catch(ArrayQueueException e) {
 			System.out.println(e.getMessage());
 		}
-		
+
 		try {
 			q.dequeue();
 		} catch(ArrayQueueException e) {
